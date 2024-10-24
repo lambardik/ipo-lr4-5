c25377a400973fb23b60185befc0e2da19929edb
даниэль <dannilboyar@gmail.com>
2024-10-24 09:42:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-list = [5, 12, 3, 18, 7, 22, 10,100]
-count = 0
+def count_greater_than(numbers, n):
 
-for number in list:
-    if number > 10:
-        count += 1
+  count = 0
+  for number in numbers:
+    if number > n:
+      count += 1
+  return count
 
+list = [5, 12, 3, 18, 7, 22, 10, 100]
+count = count_greater_than(list, 10)
 print(count)
